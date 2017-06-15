368a6e76cc8bce360301589e283fe4dba1fe8215
Lars Peter Thomsen <larsp@magenta.dk>
2017-06-15 10:23:54 +0200

    Refactoring

---
+++ src/main/java/dk/magenta/datafordeler/cvr/data/company/CompanyYearlyEmployeeNumbersData.java
@@ -0,0 +1,50 @@
+package dk.magenta.datafordeler.cvr.data.company;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lars on 15-06-17.
+ */
+@Entity
+@Table(name = "cvr_company_yearly_employees")
+public class CompanyYearlyEmployeeNumbersData extends CompanyEmployeeNumbersData {
+
+
+
+    @Column(name = "yearlyEmployeesYear")
+    private int year;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+
+
+    @Override
+    public Map<String, Object> asMap() {
+        HashMap<String, Object> map = new HashMap<>(super.asMap());
+        map.put("year", this.year);
+        return map;
+    }
+
+    /**
+     * Return a map of attributes, including those from the superclass
+     * @return
+     */
+    @JsonIgnore
+    public Map<String, Object> databaseFields() {
+        HashMap<String, Object> map = new HashMap<>(super.databaseFields());
+        map.put("year", this.year);
+        return map;
+    }
+}
