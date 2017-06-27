72471efcdec4b39710ada19ab5990eecea7da9da
Lars Peter Thomsen <larsp@magenta.dk>
2017-06-27 09:10:42 +0200

    Preliminary person parse

--- src/main/java/dk/magenta/datafordeler/cvr/records/CompanyStatusRecord.java
+++ src/main/java/dk/magenta/datafordeler/cvr/records/CompanyStatusRecord.java
@@ -1,17 +1,16 @@
 package dk.magenta.datafordeler.cvr.records;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 import dk.magenta.datafordeler.core.database.QueryManager;
 import dk.magenta.datafordeler.cvr.data.company.CompanyBaseData;
-import dk.magenta.datafordeler.cvr.data.company.CompanyEntityManager;
+import dk.magenta.datafordeler.cvr.data.participant.ParticipantBaseData;
 import dk.magenta.datafordeler.cvr.data.unversioned.CompanyStatus;
 import org.hibernate.Session;
 
 /**
  * Created by lars on 26-06-17.
  */
-public class CompanyStatusRecord extends CompanyBaseRecord {
+public class CompanyStatusRecord extends BaseRecord {
 
     @JsonProperty(value = "status")
     private String status;
@@ -20,5 +19,10 @@ public class CompanyStatusRecord extends CompanyBaseRecord {
     public void populateCompanyBaseData(CompanyBaseData baseData, QueryManager queryManager, Session session) {
         CompanyStatus status = CompanyStatus.getStatus(this.status, queryManager, session);
         baseData.setStatus(status);
+    }
+
+    @Override
+    public void populateParticipantBaseData(ParticipantBaseData baseData, QueryManager queryManager, Session session) {
+
     }
 }
