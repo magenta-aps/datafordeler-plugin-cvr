e29905de69b1e4f96bd25179a0515950678247e6
Jørgen Ulrik B. Krag <jubk@magenta-aps.dk>
2017-07-07 11:20:26 +0200

    Re-import configuration file

---
+++ src/main/java/dk/magenta/datafordeler/cvr/configuration/CvrConfiguration.java
@@ -0,0 +1,49 @@
+package dk.magenta.datafordeler.cvr.configuration;
+
+import dk.magenta.datafordeler.core.configuration.Configuration;
+import dk.magenta.datafordeler.cvr.CvrPlugin;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by lars on 16-05-17.
+ */
+@javax.persistence.Entity
+@Table(name="cvr_config")
+public class CvrConfiguration implements Configuration {
+
+    @Id
+    @Column(name = "id")
+    private final String plugin = CvrPlugin.class.getName();
+
+    // Midnight every january 1st
+    @Column
+    private String pullCronSchedule = "0 0 0 1 1 ?";
+
+    @Column
+    private String registerAddress = "http://distribution.virk.dk";
+
+    @Column
+    private String username = "";
+
+    @Column
+    private String password = "";
+
+    public String getPullCronSchedule() {
+        return this.pullCronSchedule;
+    }
+
+    public String getRegisterAddress() {
+        return this.registerAddress;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
