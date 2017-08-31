82a68b6b010c38563b5f835829a8e53bc07284f1
Lars Peter Thomsen <larsp@magenta.dk>
2017-08-31 09:13:34 +0200

    Force load participant relations

--- src/main/java/dk/magenta/datafordeler/cvr/data/shared/ParticipantRelationData.java
+++ src/main/java/dk/magenta/datafordeler/cvr/data/shared/ParticipantRelationData.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import dk.magenta.datafordeler.core.database.Identification;
 import dk.magenta.datafordeler.cvr.data.DetailData;
 import dk.magenta.datafordeler.cvr.data.participant.ParticipantEntity;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
 
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlElement;
@@ -67,5 +69,13 @@ public class ParticipantRelationData extends DetailData implements Comparable<Pa
         HashMap<String, Object> map = new HashMap<>();
         map.put("participant", this.participant);
         return map;
+    }
+
+    public void forceLoad(Session session) {
+        Hibernate.initialize(this);
+        Hibernate.initialize(this.participant);
+        for (Identification organization : this.organizations) {
+            Hibernate.initialize(organization);
+        }
     }
 }
