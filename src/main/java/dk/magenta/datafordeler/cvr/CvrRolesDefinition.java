3b9a0dd04f679151079b3c4724d9efd7d599a56e
Lars Peter Thomsen <larsp@magenta.dk>
2017-09-19 13:58:19 +0200

    Add missing role definitions

--- src/main/java/dk/magenta/datafordeler/cvr/CvrRolesDefinition.java
+++ src/main/java/dk/magenta/datafordeler/cvr/CvrRolesDefinition.java
@@ -29,12 +29,36 @@ public class CvrRolesDefinition extends RolesDefinition {
         )
     );
 
+    public static ReadCommandRole READ_CVR_PULL_ROLE = new ReadCommandRole(
+            "Pull",
+            new HashMap<String, Object>() {{
+                put("plugin", "cvr");
+            }},
+            new ReadCommandRoleVersion(
+                    1.0f,
+                    "Role that gives access to read the status of the PULL command for Cvr data"
+            )
+    );
+
+    public static StopCommandRole STOP_CVR_PULL_ROLE = new StopCommandRole(
+            "Pull",
+            new HashMap<String, Object>() {{
+                put("plugin", "cvr");
+            }},
+            new StopCommandRoleVersion(
+                    1.0f,
+                    "Role that gives access to stop the PULL command for Cvr data"
+            )
+    );
+
     @Override
     public List<SystemRole> getRoles() {
         ArrayList<SystemRole> roles = new ArrayList<>();
 
         roles.add(READ_CVR_ROLE);
         roles.add(EXECUTE_CVR_PULL_ROLE);
+        roles.add(READ_CVR_PULL_ROLE);
+        roles.add(STOP_CVR_PULL_ROLE);
 
         return roles;
     }
