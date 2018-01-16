6d02fc7f83b393bd94e9b9c84aa094b0c7332777
Lars Peter Thomsen <larsp@magenta.dk>
2018-01-16 13:57:43 +0100

    Import only records that are updated since last import

--- src/main/java/dk/magenta/datafordeler/cvr/records/CvrEntityRecord.java
+++ src/main/java/dk/magenta/datafordeler/cvr/records/CvrEntityRecord.java
@@ -1,7 +1,20 @@
 package dk.magenta.datafordeler.cvr.records;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class CvrEntityRecord extends CvrBaseRecord {
     public abstract List<CvrBaseRecord> getAll();
+
+    public List<CvrBaseRecord> getSince(OffsetDateTime time) {
+        ArrayList<CvrBaseRecord> newer = new ArrayList<>();
+        for (CvrBaseRecord record : this.getAll()) {
+            OffsetDateTime recordTime = record.getLastUpdated();
+            if (recordTime == null || time == null || !recordTime.isBefore(time)) {
+                newer.add(record);
+            }
+        }
+        return newer;
+    }
 }
