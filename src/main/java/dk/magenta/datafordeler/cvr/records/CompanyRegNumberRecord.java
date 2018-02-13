01eb51c47952aee422c0f5598f874c72bc0aa2b3
Lars Peter Thomsen <larsp@magenta.dk>
2018-02-13 15:53:34 +0100

    Add missing fields

---
+++ src/main/java/dk/magenta/datafordeler/cvr/records/CompanyRegNumberRecord.java
@@ -0,0 +1,31 @@
+package dk.magenta.datafordeler.cvr.records;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dk.magenta.datafordeler.core.database.DatabaseEntry;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+/**
+ * Record for Company, CompanyUnit or Participant name.
+ */
+@Entity
+@Table(name = "cvr_record_company_regnumber", indexes = {
+        @Index(name = "cvr_record_regnumber_company", columnList = CompanyRegNumberRecord.DB_FIELD_COMPANY + DatabaseEntry.REF),
+})
+public class CompanyRegNumberRecord extends CvrBitemporalDataRecord {
+
+    public static final String DB_FIELD_REGNUMBER = "regNumber";
+    public static final String IO_FIELD_REGNUMBER = "regNummer";
+
+    @Column(name = DB_FIELD_REGNUMBER)
+    @JsonProperty(value = IO_FIELD_REGNUMBER)
+    private String regNumber;
+
+    public String getRegNumber() {
+        return this.regNumber;
+    }
+
+}
