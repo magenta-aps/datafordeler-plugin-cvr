b5d7c731d1036d81b1d926ef5f07a6bb641a0068
madsmj <madsmj@gmail.com>
2020-02-04 09:05:44 +0100

    testdata

--- src/test/java/dk/magenta/datafordeler/cvr/ParseTest.java
+++ src/test/java/dk/magenta/datafordeler/cvr/ParseTest.java
@@ -10,6 +10,7 @@ import dk.magenta.datafordeler.core.io.ImportMetadata;
 import dk.magenta.datafordeler.cvr.entitymanager.CompanyEntityManager;
 import dk.magenta.datafordeler.cvr.entitymanager.CompanyUnitEntityManager;
 import dk.magenta.datafordeler.cvr.entitymanager.ParticipantEntityManager;
+import dk.magenta.datafordeler.cvr.query.CompanyRecordQuery;
 import dk.magenta.datafordeler.cvr.records.CompanyRecord;
 import dk.magenta.datafordeler.cvr.records.CompanyUnitRecord;
 import dk.magenta.datafordeler.cvr.records.ParticipantRecord;
@@ -19,15 +20,19 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.OffsetDateTime;
 import java.util.HashMap;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Application.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ParseTest {
 
     @Autowired
@@ -68,6 +73,55 @@ public class ParseTest {
             QueryManager.clearCaches();
         }
     }
+
+    @Test
+    public void testParseCompanyDemoFile() throws DataFordelerException, IOException {
+        ImportMetadata importMetadata = new ImportMetadata();
+
+        try(Session session = sessionManager.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            importMetadata.setSession(session);
+            InputStream input = ParseTest.class.getResourceAsStream("/GLBASETEST.json");
+            JsonNode root = objectMapper.readTree(input);
+            JsonNode itemList = root.get("hits").get("hits");
+            Assert.assertTrue(itemList.isArray());
+            for (JsonNode item : itemList) {
+                String type = item.get("_type").asText();
+                CompanyEntityManager entityManager = (CompanyEntityManager) plugin.getRegisterManager().getEntityManager(schemaMap.get(type));
+                entityManager.parseData(item.get("_source").get("Vrvirksomhed"), importMetadata, session);
+            }
+            transaction.commit();
+        }
+
+        try(Session session = sessionManager.getSessionFactory().openSession()) {
+
+            CompanyRecordQuery query = new CompanyRecordQuery();
+            OffsetDateTime time = OffsetDateTime.now();
+            query.setRegistrationFromBefore(time);
+            query.setRegistrationToAfter(time);
+            query.setEffectToAfter(time);
+            query.setEffectFromBefore(time);
+            query.applyFilters(session);
+
+            List<CompanyRecord> companyList = QueryManager.getAllEntities(session, query, CompanyRecord.class);
+
+            for(CompanyRecord company : companyList) {
+
+                System.out.println("CVR "+company.getCvrNumber());
+                System.out.println("METANAME "+company.getMetadata().getNewestName().iterator().next().getName());
+                System.out.println("NAME "+company.getNames().iterator().next().getName());
+
+                if(company.getCompanyStatus().size()>0) {
+                    System.out.println(company.getCompanyStatus().iterator().next().getStatus());
+                }
+                if(company.getPostalAddress().size()>0) {
+                    System.out.println(company.getPostalAddress().iterator().next().getMunicipality().getMunicipalityCode());
+                }
+                System.out.println();
+            }
+        }
+    }
+
 
     @Test
     public void testParseUnitFile() throws IOException, DataFordelerException {
