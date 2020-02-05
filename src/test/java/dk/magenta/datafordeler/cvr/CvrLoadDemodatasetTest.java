155334ed7ebf70776c65c1dce5c720a96f143b03
madsmj <madsmj@gmail.com>
2020-02-05 14:13:52 +0100

    ad cleaning to be activated from pull

--- src/test/java/dk/magenta/datafordeler/cvr/CvrLoadDemodatasetTest.java
+++ src/test/java/dk/magenta/datafordeler/cvr/CvrLoadDemodatasetTest.java
@@ -1,10 +1,15 @@
 package dk.magenta.datafordeler.cvr;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import dk.magenta.datafordeler.core.Application;
+import dk.magenta.datafordeler.core.Engine;
+import dk.magenta.datafordeler.core.Pull;
 import dk.magenta.datafordeler.core.database.QueryManager;
 import dk.magenta.datafordeler.core.database.SessionManager;
 import dk.magenta.datafordeler.core.exception.DataFordelerException;
 import dk.magenta.datafordeler.core.io.ImportMetadata;
+import dk.magenta.datafordeler.core.plugin.RegisterManager;
 import dk.magenta.datafordeler.cvr.entitymanager.CvrEntityManager;
 import dk.magenta.datafordeler.cvr.query.CompanyRecordQuery;
 import dk.magenta.datafordeler.cvr.records.CompanyRecord;
@@ -29,6 +34,8 @@ import java.net.URL;
 import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.List;
+
+import static org.mockito.Matchers.any;
 
 /**
  * Test that it is possible to load and clear data which is dedicated for demopurpose
@@ -149,4 +156,63 @@ public class CvrLoadDemodatasetTest {
             Assert.assertEquals(0, companyList.size());
         }
     }
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private CvrPlugin plugin;
+
+    @Autowired
+    private Engine engine;
+
+
+    /**
+     * Verify that testdata can be cleaned through calling pull with flag "cleantestdatafirst":true
+     * @throws Exception
+     */
+    @Test
+    public void testCleanTestdataThroughPull() throws Exception {
+
+        when(plugin.getRegisterManager(), registerManager, "/EMPTYGLBASETEST.json");
+        //Clean the testdata
+        ObjectNode config = (ObjectNode) objectMapper.readTree("{\"plugin\":\"cpr\",\"remote\":false,\"cleantestdatafirst\":true}");
+        Pull pull = new Pull(engine, plugin, config);
+        pull.run();
+
+        try(Session session = sessionManager.getSessionFactory().openSession()) {
+            List<CompanyRecord> personEntities = QueryManager.getAllEntities(session, CompanyRecord.class);
+            Assert.assertEquals(0, personEntities.size());//Validate that 0 company from the file persondata is initiated
+        }
+
+        when(plugin.getRegisterManager(), registerManager, "/GLBASETEST.json");
+        //Clean the testdata
+        config = (ObjectNode) objectMapper.readTree("{\"plugin\":\"cpr\",\"remote\":false,\"cleantestdatafirst\":true}");
+        pull = new Pull(engine, plugin, config);
+        pull.run();
+
+        try(Session session = sessionManager.getSessionFactory().openSession()) {
+            List<CompanyRecord> personEntities = QueryManager.getAllEntities(session, CompanyRecord.class);
+            Assert.assertEquals(4, personEntities.size());//Validate that 4 company from the file persondata is initiated
+        }
+
+        when(plugin.getRegisterManager(), registerManager, "/EMPTYGLBASETEST.json");
+        //Clean the testdata
+        config = (ObjectNode) objectMapper.readTree("{\"plugin\":\"cpr\",\"remote\":false,\"cleantestdatafirst\":true}");
+        pull = new Pull(engine, plugin, config);
+        pull.run();
+
+        try(Session session = sessionManager.getSessionFactory().openSession()) {
+            List<CompanyRecord> personEntities = QueryManager.getAllEntities(session, CompanyRecord.class);
+            Assert.assertEquals(0, personEntities.size());//Validate that 0 company from the file persondata is initiated
+        }
+    }
+
+    private void when(RegisterManager registerManager, CvrRegisterManager registerManager1, String testSet) throws URISyntaxException {
+        registerManager1.setCvrDemoFile("88888881,88888882,88888883,88888884");
+        URL testData = ParseTest.class.getResource(testSet);
+        String testDataPath = testData.toURI().toString();
+        registerManager1.setCvrDemoFile(testDataPath);
+    }
+
 }
